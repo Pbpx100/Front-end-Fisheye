c71862068e0c672703074db0d07987081d324d60
Paul B. P <38941153+Pbpx100@users.noreply.github.com>
2024-01-05 21:01:53 +0100

    Update contactForm.js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,60 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
-}
 
-function closeModal() {
+//Applying the contact form contactez-moi
+function contactFormFunc() {
+
     const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+    const btnDispContModal = document.getElementById('btn_disp_cont_modal')
+    const btnCloseModalCont = document.getElementById("btn_close_cont_modal")
+    const ButtonDataSend = document.getElementById('btn_cont_send')
+
+
+
+    function displayModal() {
+        modal.style.display = "flex";
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+    }
+    function contactData(e) {
+        e.preventDefault();
+        var formData = document.getElementById('contact-form')
+        var elements = formData.elements;
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            console.log(element.value);
+            element.value = ''
+            closeModal()
+
+        }
+    }
+    btnCloseModalCont.addEventListener("click", (e) => {
+        closeModal()
+    });
+    btnCloseModalCont.addEventListener('keyup', (e) => {
+        if (e.key == 'Enter' || e.key == 13) {
+            closeModal()
+        }
+    });
+
+    btnDispContModal.addEventListener("click", () => {
+        displayModal()
+
+    });
+
+    btnDispContModal.addEventListener("keyup", (e) => {
+        if (e.key == 'Enter') {
+            displayModal()
+        }
+    });
+
+    ButtonDataSend.addEventListener("click", (e) => {
+        contactData(e);
+    });
+    ButtonDataSend.addEventListener("keyup", (e) => {
+        if (e.key == 'Enter') {
+            contactData(e);
+        }
+
+    });
 }
