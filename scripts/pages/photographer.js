1823029297470fe6cbc96270d3fdacac6fe697c7
Paul B. P <38941153+Pbpx100@users.noreply.github.com>
2024-01-05 20:59:13 +0100

    Update photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,15 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    try {
+        const response = await fetch('data/photographers.json')
+        if (!response.ok) {
+            throw new Error('error', error)
+        }
+        const data = await response.json();
+        const photographers = data
+        //console.log(photographers)
+        return photographers
+    } catch (error) {
+        console.error('error en el fetch')
+        throw error;
+    }
+}
